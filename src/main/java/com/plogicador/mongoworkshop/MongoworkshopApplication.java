89c3f3bfc98aedcf00743e851c754f890b4299be
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-26 17:02:53 -0300

    feat: First commit - create project

---
+++ src/main/java/com/plogicador/mongoworkshop/MongoworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.plogicador.mongoworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plogicador/mongoworkshop/MongoworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plogicador.mongoworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
