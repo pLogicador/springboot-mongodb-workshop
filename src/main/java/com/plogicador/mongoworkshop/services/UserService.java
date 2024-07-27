4e6c553751904528221b7a6abe9fb1b83fb26fc4
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-27 02:58:59 -0300

    feat: User insertion with POST

--- src/main/java/com/plogicador/mongoworkshop/services/UserService.java
+++ src/main/java/com/plogicador/mongoworkshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.plogicador.mongoworkshop.domain.User;
+import com.plogicador.mongoworkshop.dto.UserDTO;
 import com.plogicador.mongoworkshop.repository.UserRepository;
 import com.plogicador.mongoworkshop.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
