63ee66a228a0e6d2d54cd8ad6ffb21de73f5c1c6
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-27 04:23:24 -0300

    feat: Getting a post by ID

---
+++ src/main/java/com/plogicador/mongoworkshop/services/PostService.java
@@ -0,0 +1,23 @@
+package com.plogicador.mongoworkshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.plogicador.mongoworkshop.domain.Post;
+import com.plogicador.mongoworkshop.repository.PostRepository;
+import com.plogicador.mongoworkshop.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+}
