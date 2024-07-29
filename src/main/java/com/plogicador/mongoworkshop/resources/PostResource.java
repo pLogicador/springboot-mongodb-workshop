903297a5be5797f95d4662aa4968916b5b004c21
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-29 13:34:59 -0300

    feat: Consultation with multiple criteria

--- src/main/java/com/plogicador/mongoworkshop/resources/PostResource.java
+++ src/main/java/com/plogicador/mongoworkshop/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.plogicador.mongoworkshop.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,4 +34,17 @@ public class PostResource {
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/multiplesearch")
+	public ResponseEntity<List<Post>> multipleSearch(
+			@RequestParam(value = "text", defaultValue= "") String text,
+			@RequestParam(value = "minDate", defaultValue= "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue= "") String maxDate) {
+		text= URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.multipleSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
