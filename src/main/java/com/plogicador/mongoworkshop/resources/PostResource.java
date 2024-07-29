07576cd0a91bbd2909b0b19f388704c4c8b65fd8
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-29 10:22:21 -0300

    feat: Simple queries with query methods

--- src/main/java/com/plogicador/mongoworkshop/resources/PostResource.java
+++ src/main/java/com/plogicador/mongoworkshop/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.plogicador.mongoworkshop.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.plogicador.mongoworkshop.domain.Post;
+import com.plogicador.mongoworkshop.resources.util.URL;
 import com.plogicador.mongoworkshop.services.PostService;
 
 @RestController
@@ -23,4 +27,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTile(@RequestParam(value = "text", defaultValue = "") String text) {
+		text= URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
